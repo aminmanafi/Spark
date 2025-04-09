f673a6786c02363479c2576e501ce84e93765e0c
Amin Manafi <amin.manafi75@yahoo.com>
2025-04-09 17:18:37 +0330

    data sources

--- code/chap9-dataSources.py
+++ code/chap9-dataSources.py
@@ -55,8 +55,68 @@ csvFile.write.format("orc").mode("overwrite").save("/tmp/my-json-file.orc")
 
 ## SQL DATABASE
 # read
+# to conccet databases to spark you should add db connector jdbc
+# file and then after spark-submit you should add --jars jdbc-path
 driver = "org.sqlite.JDBC"
 path = "/data/flight-data/jdbc/my-sqlite.db"
 url = "jdbc:sqlite:" + path
 tablename = "flight_info"
+
+dbDataFrame = spark.read.format("jdbc").option("url", url)\
+                   .option("dbtable", tablename)\
+                   .option("driver", driver).load()
+
+dbDataFrame.select("DEST_COUNTRY_NAME").distinct().show(5)
+
+# read from a query
+pushdownQuery = """(SELECT DISTINCT(DEST_COUNTRY_NAME) FROM flight_info)
+AS flight_info"""
+dbDataFrame = spark.read.format("jdbc")\
+                   .option("url", url)\
+                   .option("dbtable", pushdownQuery)\
+                   .option("driver", driver)\
+                   .load()
+
+# parallel reading
+pushdownQuery = """(SELECT DISTINCT(DEST_COUNTRY_NAME) FROM flight_info)
+AS flight_info"""
+dbDataFrame = spark.read.format("jdbc")\
+                   .option("url", url)\
+                   .option("dbtable", pushdownQuery)\
+                   .option("driver", driver)\
+                   .option("numPartitions", 10)\
+                   .load()
+
+
+# defining predicates
+props = {"driver":"org.sqlite.JDBC"}
+predicates = [
+"DEST_COUNTRY_NAME = 'Sweden' OR ORIGIN_COUNTRY_NAME = 'Sweden'",
+"DEST_COUNTRY_NAME = 'Anguilla' OR ORIGIN_COUNTRY_NAME = 'Anguilla'"]
+
+spark.read.jdbc(url, tablename, predicates=predicates, properties=props).show()
+print(spark.read.jdbc(url,tablename,predicates=predicates,properties=props)\
+.rdd.getNumPartitions()) # 2
+
+# predicates with duplicate rows
+props = {"driver":"org.sqlite.JDBC"}
+predicates = [
+"DEST_COUNTRY_NAME != 'Sweden' OR ORIGIN_COUNTRY_NAME != 'Sweden'",
+"DEST_COUNTRY_NAME != 'Anguilla' OR ORIGIN_COUNTRY_NAME != 'Anguilla'"]
+spark.read.jdbc(url, tablename, predicates=predicates, properties=props).count() # 510
+
+# partitioning based on sliding window
+colName = "count"
+lowerBound = 0
+upperBound = 348113 # this is the max count in our database
+numPartitions = 10
+
+spark.read.jdbc(url, tablename, column=colName, properties=props,
+              lowerBound=lowerBound, upperBound=upperBound,
+              numPartitions=numPartitions).count() # 255
+
+# write
+newPath = "jdbc:sqlite://tmp/my-sqlite.db"
+csvFile.write.jdbc(newPath, tablename, mode="append", properties=props)
+
 
